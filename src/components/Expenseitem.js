fc1bc1c6ab4bf7900987e49e5c6c904a1e8428af
Radek <darpaterek@gmail.com>
2022-10-26 23:43:36 +0200

    Added const for dates

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,11 +1,14 @@
 import './Expenseitem.css'
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;    
+    const month = props.date.toLocaleString('en-US',{ month :'long'});
+    const day = props.date.toLocaleString('en-US',{ day :'2-digit'});
+    const year = props.date.getFullYear();  
     return (
         <div className='expense-item'>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
             <div> {props.date.toISOString()} </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
